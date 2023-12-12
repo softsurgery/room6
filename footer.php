ea507fdc444c90d10f1db90a2118522547e18a96
softsurgey <softsurgey>
2023-12-12 14:26:55 +0100

    first commit

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <div>
+    <a href="https://coreui.io">CoreUI</a>
+    <span>&copy; 2022 creativeLabs.</span>
+  </div>
+  <div>
+    <span>Powered by</span>
+    <a href="https://coreui.io">CoreUI</a>
+  </div>
+</footer>
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Monitor</title>
+    <link rel="stylesheet" href="./assets/css/coreui.min.css">
+</head>
+
+<body>
+    <?php include "navbar.php" ?>
+    <div class="container-fluid text-center">
+        <div class="row">
+            <div class="col">
+                1 of 2
+            </div>
+            <div class="col">
+                2 of 2
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                1 of 3
+            </div>
+            <div class="col">
+                2 of 3
+            </div>
+            <div class="col">
+                3 of 3
+            </div>
+        </div>
+    </div>
+    </div>
+    <?php include "footer.php" ?>
+    <script src="./assets/js/coreui.min.js"></script>
+</body>
+
+</html>
