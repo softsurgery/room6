e3293947c5dbabe7fc1b88ae22b7f17f4ef0b1a1
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2023-12-13 13:24:53 +0100

    col4_test

--- index.php
+++ index.php
@@ -21,13 +21,16 @@
         </div>
         <div class="row">
             <div class="col">
-                1 of 3
+                1 of 4
             </div>
             <div class="col">
-                2 of 3
+                2 of 4
             </div>
             <div class="col">
-                3 of 3
+                3 of 4
+            </div>
+            <div class="col">
+                4 of 4
             </div>
         </div>
     </div>
