7e69399078e38dafeece4fb59353670d7ec811db
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2023-12-15 13:43:52 +0100

    commit_test

--- index.php
+++ index.php
@@ -21,16 +21,19 @@
         </div>
         <div class="row">
             <div class="col">
-                1 of 4
+                1 of 5
             </div>
             <div class="col">
-                2 of 4
+                2 of 5
             </div>
             <div class="col">
-                3 of 4
+                3 of 5
             </div>
             <div class="col">
-                4 of 4
+                4 of 5
+            </div>
+            <div class="col">
+                5 of 5
             </div>
         </div>
     </div>
